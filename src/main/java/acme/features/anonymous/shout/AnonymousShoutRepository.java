e9173873bbd5187d4937399f543b71b87cbddfdd
pedmuncif <pedmuncif@alum.us.es>
2021-04-05 16:15:58 +0200

    Arreglos S02

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
@@ -1,12 +1,14 @@
 package acme.features.anonymous.shout;
 
 import java.util.Collection;
-import acme.entities.shouts;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import acme.entities.shouts.Shout;
 import acme.framework.repositories.AbstractRepository;
 
+@Repository
 public interface AnonymousShoutRepository extends AbstractRepository{
 	
 	@Query("select s from Shout s")
