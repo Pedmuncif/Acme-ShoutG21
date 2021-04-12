b973ae89cd21b345b9ca12dff35ea0de5b833019
enrik <enrik@DESKTOP-IFRPPJE>
2021-04-12 16:41:28 +0200

    Task 016 Finished

---
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementController.java
@@ -0,0 +1,35 @@
+package acme.features.administrator.announcement;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import acme.components.CustomCommand;
+import acme.framework.components.BasicCommand;
+import acme.framework.controllers.AbstractController;
+import acme.framework.entities.Administrator;
+
+@Controller
+@RequestMapping("/administrator/announcement/")
+public class AdministratorAnnouncementController extends AbstractController<Administrator, Announcement> {
+	
+	@Autowired
+	protected AdministratorAnnouncementListRecentService listRecentService;
+	
+	@Autowired
+	protected AdministratorAnnouncementShowService showService;
+	
+	@Autowired
+	protected AdminstratorAnnouncementCreateService createService;
+	
+	@PostConstruct
+	protected void initialise() {
+		super.addCustomCommand(CustomCommand.LIST_ALL, BasicCommand.LIST, this.listAllService);
+		super.addCustomCommand(CustomCommand.LIST_RECENT, BasicCommand.LIST, this.listRecentService);
+		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
+	}
+
+}
