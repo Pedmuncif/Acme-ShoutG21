574885860779ed114b2c30bb9ecb21d94074f13e
pedmuncif <pedmuncif@alum.us.es>
2021-04-12 16:17:25 +0200

    Task 013: Done

---
+++ src/main/java/acme/features/authenticated/announcement/AuthenticatedAnnouncementController.java
@@ -0,0 +1,33 @@
+package acme.features.authenticated.announcement;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import acme.framework.components.BasicCommand;
+import acme.framework.entities.Authenticated;
+
+@Controller
+@RequestMapping("/authenticated/announcement")
+public class AuthenticatedAnnouncementController extends AbstractController<Authenticated, Announcement> {
+	
+	// Internal state -------------------------------------
+	
+	@Autowired
+	protected AuthenticatedAnnouncementListService listService;
+	
+	@Autowired
+	protected AuthenticatedAnnouncementShowService showService;
+	
+	//Constructors --------------------------------------------
+	
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+		
+	}
+
+}
